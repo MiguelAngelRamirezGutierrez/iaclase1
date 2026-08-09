141b9d804a5c2fdaf62a90dc8a896eac421c9f6a
MiguelAngelRamirezGutierrez <ramirezps3ps42015@gmail.com>
2026-08-09 18:45:08 -0500

    condicional
    En esta agrego la condicional

--- analisis_datos.py
+++ analisis_datos.py
@@ -1,3 +1,10 @@
 # Defino las variables
 temperatura = 35
 ciudad = "Cartago"
+
+# Condicional 
+if temperatura > 35:
+    print(f"hace calor en {ciudad}")
+else:
+    print(f"hace frio en {ciudad}")
+    
